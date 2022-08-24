8946a6b08ea444c3b546cbe00ddb112149053215
Luiz Henrique <luizhenrique92sp@gmail.com>
2022-08-24 15:43:34 -0300

    Quase no final, já está salvando no localStorage e modificando

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ const form = document.querySelector("[data-formulario]")
 const lista = document.querySelector("[data-lista]")
 const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-console.log(itens)
-
 itens.forEach((elemento) => {
     criaElemento(elemento)
 })
@@ -12,14 +10,27 @@ form.addEventListener("submit", (evento) => {
     evento.preventDefault()
     const nome = evento.target.elements["nome"]
     const quantidade = evento.target.elements["quantidade"]
+    const existe = itens.find(elemento => elemento.nome === nome.value)
+    
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
+    
 
-    criaElemento(itemAtual)
+    if(existe) {
+        itemAtual.id = existe.id
 
-    itens.push(itemAtual)
+        atualizaElemento(itemAtual)
+
+        itens[existe.id] = itemAtual
+    } else {
+        itemAtual.id = itens.length
+
+        criaElemento(itemAtual)
+        
+        itens.push(itemAtual)
+    }
 
     localStorage.setItem("itens", JSON.stringify(itens))
 
@@ -30,12 +41,17 @@ form.addEventListener("submit", (evento) => {
 
 function criaElemento(item) {
     const novoItem = document.createElement("li")
-    const novoNumero = document.createElement("strong")
-    
     novoItem.classList.add("item")
+
+    const novoNumero = document.createElement("strong")
     novoNumero.innerHTML = item.quantidade
+    novoNumero.dataset.id = item.id
     novoItem.appendChild(novoNumero)
     novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
+}
+
+function atualizaElemento(item) {
+    document.querySelector(`[data-id="${item.id}"]`).innerHTML = item.quantidade
 }
